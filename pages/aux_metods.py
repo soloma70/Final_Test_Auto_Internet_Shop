2ca72a0a1d6dad2a293e9d918e11154907779dd9
soloma70 <soloma70@gmail.com>
2021-11-07 01:10:52 +0200

    Вспомагательные методы вынесены в отдельный файл

---
+++ pages/aux_metods.py
@@ -0,0 +1,17 @@
+class AuxMetods:
+    """Класс вспомагательные медоты, используемые в параметризации поиска, для добавления различных строк"""
+
+    def generate_string(n):
+        return "x" * n
+
+    def russian_chars():
+        return 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя'
+
+    def chinese_chars():
+        return '的一是不了人我在有他这为之大来以个中上们'
+
+    def special_chars():
+        return '|\\/!@#$%^&*()-_=+`~?"№;:[]{}'
+
+    def start_img_click_mobile(self):
+        self.start_img_mobile.click()
