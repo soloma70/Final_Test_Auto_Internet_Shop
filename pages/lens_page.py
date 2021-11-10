bce3b7d014a9bb0f88e3d22a85cb359117fdab27
soloma70 <soloma70@gmail.com>
2021-11-11 00:25:09 +0200

    Добавлены тесты страницы линзы

--- pages/lens_page.py
+++ pages/lens_page.py
@@ -17,4 +17,10 @@ class LensPage(BasePage):
 
         # Filter elements
         self.filters = driver.find_elements(*LensLocators.filters)
-        self.filters = driver.find_elements(*LensLocators.filters)
+        self.brands = driver.find_elements(*LensLocators.filter_list[0])
+        self.lines = driver.find_elements(*LensLocators.filter_list[1])
+        self.type_lens = driver.find_elements(*LensLocators.filter_list[2])
+        self.repl_mode = driver.find_elements(*LensLocators.filter_list[3])
+        self.base_curv = driver.find_elements(*LensLocators.filter_list[4])
+        self.diameter = driver.find_elements(*LensLocators.filter_list[5])
+        self.dioptr = driver.find_elements(*LensLocators.filter_list[6])
--- tests/test_lens_page.py
+++ tests/test_lens_page.py
@@ -1,6 +1,23 @@
 # -*- encoding=utf8 -*-
 from time import sleep
-from pages.sales_page import SalesPage
+from pages.lens_page import LensPage
 from pages.locators import LensLocators, ProductLensLocators
 from pages.url_list import LinsaUa
 from selenium.webdriver.common.action_chains import ActionChains
+
+
+def test_sales_banners_sales_page(web_driver_desktop):
+    """Тест проверяет видимость акционных баннеров и переход на соответствующие страницы акций"""
+
+    page = LensPage(web_driver_desktop, 5)
+    filters = page.filters
+    filter_list = []
+    for i in range(len(filters)):
+        filters[i].click()
+        sleep(1)
+        filter_list.append(web_driver_desktop.find_elements(*LensLocators.filter_list[i]))
+
+    print()
+    print(len(filters))
+    for k in range(len(filter_list)):
+        print(len(filter_list[k]))
