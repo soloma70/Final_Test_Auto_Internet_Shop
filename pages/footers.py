8b30fbf7c4a68689b387e982b94936903a4403eb
soloma70 <soloma70@gmail.com>
2021-11-14 22:18:06 +0200

    Добавлена страница и тесты футеров

--- pages/footers.py
+++ pages/footers.py
@@ -1,6 +1,8 @@
+from time import sleep
 from pages.base_page import BasePage
 from pages.url_list import LinsaUa
 from pages.locators import StartLocators
+from selenium.webdriver.remote.webelement import WebElement
 
 
 class Footers(BasePage):
@@ -9,9 +11,31 @@ class Footers(BasePage):
 
     def __init__(self, driver, timeout=3):
         super().__init__(driver, timeout)
-        url = LinsaUa.start_url
-        driver.get(url)
+        self.url = LinsaUa.start_url
+        driver.get(self.url)
 
-        # Footers elements
-        self.footers_left_btns = driver.find_elements(*StartLocators.footers_left_btns)
-        self.footers_right_btns = driver.find_elements(*StartLocators.footers_right_btns)
+        self.footers_left = driver.find_elements(*StartLocators.footers_left_btns)
+        self.footers_right = driver.find_elements(*StartLocators.footers_right_btns)
+
+
+    def footers_left_click(self, index: int):
+        self.driver.find_elements(*StartLocators.footers_left_btns)[index].click()
+
+    def footers_right_click(self, index: int):
+        self.driver.find_elements(*StartLocators.footers_right_btns)[index].click()
+
+    def footers_bottom_left_click(self):
+        self.driver.find_element(*StartLocators.footer_bottom_left_btn).click()
+
+    def footers_bottom_right_click(self):
+        self.driver.find_element(*StartLocators.footer_bottom_right_btn).click()
+
+    def footers_middle_click_and_goto(self, index: int) -> str:
+        self.driver.find_elements(*StartLocators.footer_middle_btns)[index].click()
+        windows = self.driver.window_handles
+        self.driver.switch_to.window(windows[1])
+        current_url = self.driver.current_url
+        self.driver.close()
+        sleep(2)
+        self.driver.switch_to.window(windows[0])
+        return current_url
---
+++ tests/test_footers.py
@@ -0,0 +1,46 @@
+# -*- encoding=utf8 -*-
+
+from pages.footers import Footers
+from pages.url_list import LinsaUa
+
+
+def test_footer_start_page(web_driver_desktop):
+    """Тест проверяет кликабельность блоков в footers
+    и корректность перехода по ссылкам """
+
+    page = Footers(web_driver_desktop, 10)
+    footers = []
+    foot_left = page.footers_left
+    foot_right = page.footers_right
+
+    # Проверка левых и правых футеров меню
+    for index in range(len(foot_left)):
+        page.footers_left_click(index)
+        footers.append(page.get_relative_link())
+        page.get_url(page.url)
+
+    for index in range(len(foot_right)):
+        page.footers_right_click(index)
+        footers.append(page.get_relative_link())
+        page.get_url(page.url)
+
+    # Проверка нижних левых и правых футеров меню
+    page.footers_bottom_left_click()
+    footers.append(page.get_relative_link())
+    page.get_url(page.url)
+
+    page.footers_bottom_right_click()
+    footers.append(page.get_relative_link())
+    page.get_url(page.url)
+
+    # Проверка кнопок Фейсбук и Инстаграм
+    for index in range(2):
+        footer = page.footers_middle_click_and_goto(index)
+        footers.append(footer)
+        page.get_url(page.url)
+
+    # Переход на страницу инстаграм не проверяется, требует логин инстаграм
+    for index in range(11):
+        assert footers[index] == LinsaUa.footers_menu_urls[index][0] or \
+                footers[index] == LinsaUa.footers_menu_urls[index][1] \
+             , f'ERROR! Bad transaction for {index}: {footers[index]}'
