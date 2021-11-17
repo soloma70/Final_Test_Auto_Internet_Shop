0598ec88a914012c538054d93eb77f76cf3cd172
soloma70 <soloma70@gmail.com>
2021-11-18 00:13:44 +0200

    Refactor

--- pages/frames_page.py
+++ pages/frames_page.py
@@ -5,7 +5,6 @@ from time import sleep
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from random import randint
 
 
 class FramesPage(BasePage):
