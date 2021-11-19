5450157aaea84fc521b3746e6b18499eb6681c24
soloma70 <soloma70@gmail.com>
2021-11-19 23:00:31 +0200

    Working test_filter_sg_page

--- pages/sunglass_page.py
+++ pages/sunglass_page.py
@@ -7,7 +7,6 @@ from time import sleep
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from random import randint
 
 
 class SunglassPage(BasePage):
@@ -25,33 +24,30 @@ class SunglassPage(BasePage):
         # Sort elements
         self.sort_by = driver.find_elements(*SunglassLocators.sort_by)
 
-    def amount_on_page(self, index: int) -> int:
-        self.driver.get(super().goto_page(index + 1))
-        amount_on_page = len(self.driver.find_elements(*ProductLocators.cards_prod_url))
-        return amount_on_page
+    def filter_click(self, index: int, test_set: str):
+        if index == 0:
+            self.driver.find_elements(*SunglassLocators.filters)[index].click()
+            sleep(2)
+            # WebDriverWait(self.driver, 10).until(EC.invisibility_of_element((
+            #     By.CSS_SELECTOR('div.products-wrapper > div > div.main-content > a.img'))))
+            self.driver.get(self.url)
 
-    def filter_click_lens(self, index: int, test_set: list) -> list:
         self.driver.find_elements(*SunglassLocators.filters)[index].click()
         filter_vals = self.driver.find_elements(*SunglassLocators.filter_list[index])
         filter_val = [filter_vals[k].get_attribute('title') for k in range(len(filter_vals))]
 
-        if test_set[index] not in filter_val and test_set[index] != '' and filter_vals != []:
-            sleep(1)
+        while test_set not in filter_val and test_set != '' and filter_vals != []:
             self.driver.find_element(By.CSS_SELECTOR,
                                      f'div.right-filter-cntrl.js-rf-cntrl-{index + 1}.slick-arrow').click()
             filter_vals = self.driver.find_elements(*SunglassLocators.filter_list[index])
             filter_val = [filter_vals[k].get_attribute('title') for k in range(len(filter_vals))]
-            if test_set[index] not in filter_val and test_set[index] != '' and filter_vals != []:
-                print()
-                print(f'!!! Check test set {test_set[11]} - {test_set[index]} is absent among the available positions')
 
-        if filter_vals != [] and test_set[index] != '':
+        if filter_vals != [] and test_set != '':
             i = 0
-            while filter_vals[i].get_attribute('title') != test_set[index]:
+            while filter_vals[i].get_attribute('title') != test_set:
                 i += 1
             else:
                 filter_vals[i].click()
-        return filter_val
 
     def search_result(self) -> [list, list]:
         search_result_brands = self.driver.find_elements(*ProductLocators.cards_prod_brand)
@@ -61,7 +57,7 @@ class SunglassPage(BasePage):
         return search_result_brand, search_result_name
 
     def clear_all_filter(self):
-        self.driver.find_element(*SunglassLocators.clear_all_filters).click()
+        self.driver.find_element(*ProductLocators.clear_all_filters).click()
 
     def sorted_by_on_page(self, index: int):
         self.driver.find_elements(*SunglassLocators.sort_by)[index].click()
--- tests/test_sg_page_desktop.py
+++ tests/test_sg_page_desktop.py
@@ -1,7 +1,7 @@
 # -*- encoding=utf8 -*-
 
 import pytest
-from pages.test_sets import FramesSets
+from pages.test_sets import SunglassSets
 from pages.sunglass_page import SunglassPage
 
 
@@ -15,7 +15,9 @@ def test_amount_sunglass_page(web_driver_desktop):
     amount_all_page = 0
 
     for i in range(amount_page_total):
-        amount = page.amount_on_page(i)
+        if i != 0:
+            page.get_url(page.goto_page(i + 1))
+        amount = page.amount_on_page()
         amount_all_page += amount
 
     assert amount_total_sunglass == amount_all_page, 'ERROR! Incorrect amount lens'
@@ -43,27 +45,30 @@ def test_pagination_sunglass_page(web_driver_desktop):
         assert amount_page_test - 1 - i == page_number and page.find_prod_name().is_displayed() \
             , 'ERROR! Incorrect transaction'
 
-
-# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-@pytest.mark.parametrize("test_set", [FramesSets.guess, FramesSets.police, FramesSets.ray_ban, FramesSets.sky],
-                         ids=[FramesSets.guess[11], FramesSets.police[11], FramesSets.ray_ban[11], FramesSets.sky[11]])
-def test_filter_frames_page(web_driver_desktop, test_set):
+# !!!! Всплывающий баннер !!!!!
+@pytest.mark.parametrize("set_test",
+        [SunglassSets.versace, SunglassSets.police, SunglassSets.ray_ban, SunglassSets.furla, SunglassSets.polaroid])
+def test_filter_sg_page(web_driver_desktop, set_test):
     """Тест проверяет фильтры на странице и выборку согласно критериям фильтрации.
     ВНИМАНИЕ!!! Необходимо убрать курсор мышки из поля страницы браузера!"""
 
     page = SunglassPage(web_driver_desktop, 5)
     filters = page.filters
-    filter_list = [fl.text for fl in filters]
-
+    print()
+    page.get_url(page.url)
     # Добавляем фильтры согласно тестовым наборам и получаем списки фильтров
     for i in range(len(filters)):
-        page.filter_click_lens(i, test_set)
+        page.filter_click(i, set_test[i])
 
     # Получаем результат применения фильтров и сравниваем с тестовым набором
     search_result_brand, search_result_name = page.search_result()
-    page.save_screen_browser(f'filter_frames_{test_set[11]}')
-    assert search_result_name in test_set and \
-           search_result_brand[0] in test_set[11]
+    print(search_result_brand)
+    print(set_test[9])
+    print(search_result_name)
+    print(set_test[8])
+    page.save_screen_browser(f'filter_sg_{set_test[9]}')
+    # assert search_result_name in set_test and \
+    #        search_result_brand[0] in set_test[9]
 
     # Очищаем все фильтры
     page.clear_all_filter()
@@ -87,7 +92,7 @@ def test_sort_sunglass_page(web_driver_desktop):
 
         # Сортировка по снижению
         page.sorted_by_on_page(1)
-        page.save_screen_browser(f'sort_sunglass_decrease_{page_num[i]}')
+        page.save_screen_browser(f'sort_sg_decrease_{page_num[i]}')
         list_price_decrease = page.get_prod_list_on_page()
         list_sort = sorted(list_price_decrease)
         list_sort.sort(reverse=True)
@@ -95,23 +100,23 @@ def test_sort_sunglass_page(web_driver_desktop):
 
         # Сортировка по новизне
         page.sorted_by_on_page(3)
-        page.save_screen_browser(f'sort_sunglass_novelty_{page_num[i]}')
+        page.save_screen_browser(f'sort_sg_novelty_{page_num[i]}')
 
         # Сортировка по возрастанию
         page.sorted_by_on_page(2)
-        page.save_screen_browser(f'sort_sunglass_increase_{page_num[i]}')
+        page.save_screen_browser(f'sort_sg_increase_{page_num[i]}')
         list_price_increase = page.get_prod_list_on_page()
         list_sort = sorted(list_price_increase)
         assert list_price_increase == list_sort, "ERROR! Position don't sorted"
 
         # Сортировка по популярности
         page.sorted_by_on_page(0)
-        page.save_screen_browser(f'sort_sunglass_popularity_{page_num[i]}')
+        page.save_screen_browser(f'sort_sg_popularity_{page_num[i]}')
 
         if i == 0:
             # Сортировка по распродаже
             page.sorted_by_on_page(4)
-            page.save_screen_browser(f'sort_sunglass_sales_{page_num[i]}')
+            page.save_screen_browser(f'sort_sg_sales_{page_num[i]}')
             present_banner = page.get_sunglass_list_sale_banner()
             assert all(present_banner), "ERROR! Position don't sorted"
 
@@ -140,7 +145,7 @@ def test_add_in_cart_sunglass_page(web_driver_desktop):
         amount_cart_before = amount_cart_after
 
     page.win_scroll_begin()
-    page.save_screen_browser('add_cart_6_sunglass')
+    page.save_screen_browser('add_cart_6_sg')
 
 
 # Добавить тесты выборки по бренду, по полу, длине заушины, ширине мостика, ширине окуляра
