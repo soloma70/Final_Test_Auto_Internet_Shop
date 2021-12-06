f13d6c30fd10a4e3976f6a5d462de0d9180aae19
soloma70 <soloma70@gmail.com>
2021-12-07 01:25:49 +0200

    Add static methods

--- pages/aux_metods.py
+++ pages/aux_metods.py
@@ -1,34 +1,41 @@
 from random import choice
 from string import ascii_letters, digits
 
-class AuxMetods:
-    """Класс вспомагательные методы, используемые в параметризации поиска, для добавления различных строк"""
 
+class AuxMetods:
+    """Класс вспомагательных методов, которые используются в параметризации, для добавления различных строк и цифр"""
+
+    @staticmethod
     def generate_string(n: int) -> str:
         return "x" * n
 
+    @staticmethod
     def russian_chars() -> str:
         return 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя'
 
+    @staticmethod
     def english_chars() -> str:
         return 'abcdefghijklmnopqrstuvwxyz'
 
+    @staticmethod
     def random_chars(length: int) -> str:
         random_str = ''.join(choice(ascii_letters) for i in range(length))
         return random_str
 
+    @staticmethod
     def random_num(length: int) -> int:
         random_int = int(''.join(choice(digits) for i in range(length)))
         return random_int
 
+    @staticmethod
     def random_phone(length: int) -> str:
-        rand_phone = f"73{''.join(choice(digits) for i in range(length-2))}"
+        rand_phone = f"73{''.join(choice(digits) for i in range(length - 2))}"
         return rand_phone
 
+    @staticmethod
     def chinese_chars() -> str:
         return '的一是不了人我在有他这为之大来以个中上们'
 
+    @staticmethod
     def special_chars() -> str:
         return '|\\/!@#$%^&*()-_=+`~?"№;:[]{}'
-
-
